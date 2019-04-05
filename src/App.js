63836f57e5a412d8dcfc978fe37d9e3083ee3bba
blc132 <pawelkobrynski97@gmail.com>
2019-04-05 19:35:42 +0200

    added onSave function, fixed styles

--- src/App.js
+++ src/App.js
@@ -5,21 +5,37 @@ import { BASE_URL } from './constans';
 import axios from 'axios';
 
 class App extends Component {
+
+
+  onSave = () => {
+    console.log(document.getElementById('textPattern').value)
+    axios.post(BASE_URL + '/home/',
+        { textPattern: document.getElementById('textPattern').value, 
+          textsToCompare: document.getElementById('textsToCompare').value,
+          splitText: document.getElementById('splitText').value }
+    ).then(() => {
+        console.log("Sukces")
+    }).catch((error) => {
+        console.log(error);
+        window.alert("Błąd podczas wysyłania żądania")
+    });
+}
+
+
   render() {
-    return (
-      
+    return (      
       <div className="App">
           <div className="left-container">
           <label>Tekst wzorcowy</label>
-          <textarea className="textPattern" rows="11" cols="25" placeholder="tekst wzorcowy"></textarea>
-          <button className="cancelButton" class="btn btn-danger">Anuluj</button>
+          <textarea id="textPattern" className="textPattern" rows="11" cols="25" placeholder="tekst wzorcowy"></textarea>
+          <button className="btn btn-danger">Anuluj</button>
           </div>
           <img src={logo} className="App-logo" alt="logo" />
           <div className="right-container">
           <label>Teksty do porównania</label>
-          <textarea className="textsToCompare" rows="10" cols="25" placeholder="teksty do porównania"></textarea>
-          <input className="splitText" placeholder="tekst rozdzielający"></input>
-          <button class="btn btn-primary">Porównaj</button>
+          <textarea id="textsToCompare" className="textsToCompare" rows="10" cols="25" placeholder="teksty do porównania"></textarea>
+          <input id="splitText" className="splitText" placeholder="tekst rozdzielający"></input>
+          <button className="btn btn-primary" onClick={this.onSave}>Porównaj</button>
           </div>
       </div>
     );
