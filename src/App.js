a1f6f4f1a9c426ba546ca6e4202fb7975f4a6f06
blc132 <pawelkobrynski97@gmail.com>
2019-04-25 23:45:33 +0200

    added app state, added apiAdress input

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BASE_URL } from './constans';
 import axios from 'axios';
 
 class App extends Component {
 
+  state = {
+    baseUrl: "",
+    textPattern: "",
+    textsToCompare: "",
+    splitText: ""
+  };
 
   onSave = () => {
-    console.log(document.getElementById('textPattern').value)
-    axios.post(BASE_URL + '/home/',
-        { textPattern: document.getElementById('textPattern').value, 
-          textsToCompare: document.getElementById('textsToCompare').value,
-          splitText: document.getElementById('splitText').value }
-    ).then(() => {
-        console.log("Sukces")
+    this.baseUrl = document.getElementById('apiAdress').value;
+    this.textPattern = document.getElementById('textPattern').value;
+    this.textsToCompare = document.getElementById('textsToCompare').value;
+    this.splitText = document.getElementById('splitText').value;
+
+    console.log(this.baseUrl)
+    axios.post(this.baseUrl + '/home/',
+      {
+        textPattern: this.textPattern,
+        textsToCompare: this.textsToCompare,
+        splitText: this.splitText
+      }
+    ).then((response) => {
+      console.log("Sukces")
+      console.log(response)
     }).catch((error) => {
-        console.log(error);
-        window.alert("Błąd podczas wysyłania żądania")
+      console.log(error);
+      window.alert("Błąd podczas wysyłania żądania.")
     });
-}
+  }
 
 
   render() {
-    return (      
+    return (
       <div className="App">
-          <div className="left-container">
+        <div className="left-container">
           <label>Tekst wzorcowy</label>
-          <textarea id="textPattern" className="textPattern" rows="11" cols="25" placeholder="tekst wzorcowy"></textarea>
+          <textarea id="textPattern" className="textPattern" rows="12" cols="25" placeholder="tekst wzorcowy"></textarea>
           <button className="btn btn-danger">Anuluj</button>
-          </div>
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="right-container">
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="right-container">
           <label>Teksty do porównania</label>
           <textarea id="textsToCompare" className="textsToCompare" rows="10" cols="25" placeholder="teksty do porównania"></textarea>
+          <input id="apiAdress" className="apiAdress" placeholder="adres API"></input>
           <input id="splitText" className="splitText" placeholder="tekst rozdzielający"></input>
           <button className="btn btn-primary" onClick={this.onSave}>Porównaj</button>
-          </div>
+        </div>
       </div>
     );
   }
