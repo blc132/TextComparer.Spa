9c3821885f8524308104a85f68f1d49619390b01
blc132 <pawelkobrynski97@gmail.com>
2019-04-26 16:38:47 +0200

    moved homepage to HomePage.js, added result textare

--- src/App.js
+++ src/App.js
@@ -1,57 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import { Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./components/HomePage";
 
 class App extends Component {
-
-  state = {
-    baseUrl: "",
-    textPattern: "",
-    textsToCompare: "",
-    splitText: ""
-  };
-
-  onSave = () => {
-    this.baseUrl = document.getElementById('apiAdress').value;
-    this.textPattern = document.getElementById('textPattern').value;
-    this.textsToCompare = document.getElementById('textsToCompare').value;
-    this.splitText = document.getElementById('splitText').value;
-
-    console.log(this.baseUrl)
-    axios.post(this.baseUrl + '/home/',
-      {
-        textPattern: this.textPattern,
-        textsToCompare: this.textsToCompare,
-        splitText: this.splitText
-      }
-    ).then((response) => {
-      console.log("Sukces")
-      console.log(response)
-    }).catch((error) => {
-      console.log(error);
-      window.alert("Błąd podczas wysyłania żądania.")
-    });
-  }
-
-
   render() {
     return (
-      <div className="App">
-        <div className="left-container">
-          <label>Tekst wzorcowy</label>
-          <textarea id="textPattern" className="textPattern" rows="12" cols="25" placeholder="tekst wzorcowy"></textarea>
-          <button className="btn btn-danger">Anuluj</button>
+      <Router>
+        <div className="App">
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+            </Switch>
+          </div>
         </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="right-container">
-          <label>Teksty do porównania</label>
-          <textarea id="textsToCompare" className="textsToCompare" rows="10" cols="25" placeholder="teksty do porównania"></textarea>
-          <input id="apiAdress" className="apiAdress" placeholder="adres API"></input>
-          <input id="splitText" className="splitText" placeholder="tekst rozdzielający"></input>
-          <button className="btn btn-primary" onClick={this.onSave}>Porównaj</button>
-        </div>
-      </div>
+      </Router>
     );
   }
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import './HomePage.css';
+import axios from 'axios';
+
+class HomePage extends Component {
+
+  state = {
+    baseUrl: "",
+    textPattern: "",
+    textsToCompare: "",
+    splitText: ""
+  };
+
+  onSave = () => {
+    this.baseUrl = document.getElementById('apiAdress').value;
+    this.textPattern = document.getElementById('textPattern').value;
+    this.textsToCompare = document.getElementById('textsToCompare').value;
+    this.splitText = document.getElementById('splitText').value;
+
+    console.log(this.baseUrl)
+    axios.post(this.baseUrl + '/home/',
+      {
+        textPattern: this.textPattern,
+        textsToCompare: this.textsToCompare,
+        splitText: this.splitText
+      }
+    ).then((response) => {
+      console.log("Sukces")
+      console.log(response)
+      document.getElementById('result').value = response.data;
+    //   alert(response);
+    }).catch((error) => {
+      console.log(error);
+      window.alert("Błąd podczas wysyłania żądania.")
+    });
+  }
+
+  render() {
+    return (
+        <div className="HomePage">
+            <div className="left-container">
+                <label>Tekst wzorcowy</label>
+                <textarea id="textPattern" className="textPattern" rows="13" cols="25" placeholder="tekst wzorcowy"></textarea>
+                <button className="btn btn-danger">Anuluj</button>
+            </div>
+            <img src={logo} className="App-logo" alt="logo" />
+                <div className="right-container">
+                <label>Teksty do porównania</label>
+                <textarea id="textsToCompare" className="textsToCompare" rows="13" cols="25" placeholder="teksty do porównania"></textarea>
+                <input id="splitText" className="splitText" placeholder="tekst rozdzielający"></input>
+                <input id="apiAdress" defaultValue="https://localhost:44309/api/" className="apiAdress" placeholder="adres API"></input>
+                <button className="btn btn-primary" onClick={this.onSave}>Porównaj</button>
+            </div>
+            <div className="down-container">
+                <textarea id="result" className="result" placeholder="wynik"></textarea>
+            </div>
+        </div>
+    );
+  }
+}
+
+export default HomePage;
